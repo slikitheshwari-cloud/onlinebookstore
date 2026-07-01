81f7a0b58919cf60a8f85f613c50d7dbd50c8af1
S.Likitheshwari <slikitheshwari@gmail.com>
2026-07-01 11:30:00 +0530

    Add media URL handling in DEBUG mode

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
