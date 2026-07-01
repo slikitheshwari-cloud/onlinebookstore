2370bd5e5607624298554f9b8d3565844349d97d
S.Likitheshwari <slikitheshwari@gmail.com>
2026-07-01 11:03:51 +0530

    Configure static files storage with Whitenoise
    Added STATICFILES_STORAGE setting for Whitenoise.

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -148,12 +148,15 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+ Static files (CSS, JavaScript, Images)
+ https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "files/")
